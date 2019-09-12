e5242d56f8ec95945eccd380da5316c927cd2317
Bjørnar Luteberget <bjlut@railcomplete.no>
2019-09-12 13:22:42 +0200

    move external dependencies repos into lib/

--- ci/script.sh
+++ ci/script.sh
@@ -2,5 +2,11 @@
 
 set -ex
 
+cd lib/
+git clone https://github.com/luteberget/lsqr-rs.git
+git clone https://github.com/luteberget/railplot.git
+git clone https://github.com/koengit/trainspotting.git
+cd ..
+
 cargo test --target $TARGET
 cargo build --target $TARGET --release
