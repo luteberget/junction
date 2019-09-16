53c89c621b80eb4844efd7e524388df608c75ec4
Bjørnar Luteberget <luteberget@gmail.com>
2019-09-16 21:46:10 +0200

    tikz animation

--- src/gui/mainmenu.rs
+++ src/gui/mainmenu.rs
@@ -1,3 +1,4 @@
+use nalgebra_glm as glm;
 use backend_glfw::imgui::*;
 use const_cstr::*;
 use log::*;
@@ -36,6 +37,9 @@ pub fn main_menu(app :&mut App) {
                     app.document.fileinfo.update_window_title();
                 }
 
+                if igMenuItemBool(const_cstr!("Save animation").as_ptr(), std::ptr::null(), false, true) {
+                    saveanimation(app);
+                }
                 if igMenuItemBool(const_cstr!("Load file...").as_ptr(), std::ptr::null(), false, true) {
 
                     load(app);
@@ -133,4 +137,142 @@ pub fn main_menu(app :&mut App) {
         }
     }
 }
+
+
+pub fn saveanimation(app :&mut App) {
+    use std::fmt::Write;
+    let doc = &app.document;
+    let model = doc.analysis.model();
+    let data = doc.analysis.data();
+    let (_,dgraph) = data.dgraph.as_ref().unwrap();
+    let (_,dispatch) = data.dispatch[0].as_ref().unwrap();
+
+    let mut output = String::new();
+
+    let total_time = dispatch.time_interval.1;
+    let num_frames = 100;
+    use matches::*;
+    for i in -1..(num_frames+1) {
+        let t = (i as f32) / ((num_frames-1) as f32) * total_time;
+        use crate::document::dispatch::*;
+        let instant = Instant::from(t, &dispatch.history, &dgraph);
+        writeln!(output, "%% time = {}, step = {}", t, i);
+        writeln!(output, "\\begin{{tikzpicture}}");
+        writeln!(output, "");
+
+        let mut min_x = std::f32::INFINITY;
+        let mut max_x = -std::f32::INFINITY;
+        let mut min_y = std::f32::INFINITY;
+        let mut max_y = -std::f32::INFINITY;
+        for l in &model.linesegs  {
+            writeln!(output, "\\draw [rail] ({},{}) -- ({},{});",
+                                (l.0).x,(l.0).y,(l.1).x,(l.1).y);
+            min_x = min_x.min((l.0).x as f32 );
+            min_x = min_x.min((l.1).x as f32 );
+            max_x = max_x.max((l.0).x as f32 );
+            max_x = max_x.max((l.1).x as f32 );
+            min_y = min_y.min((l.0).y as f32 );
+            min_y = min_y.min((l.1).y as f32 );
+            max_y = max_y.max((l.0).y as f32 );
+            max_y = max_y.max((l.1).y as f32 );
+        }
+        writeln!(output, "\\draw[draw=none,fill=none] ({},{}) -- ({},{});",
+                min_x-0.5, min_y-0.5, max_x+0.5,max_y+0.5);
+
+        for (pta,obj) in &model.objects {
+            let tangent = glm::normalize(&glm::vec2(obj.tangent.x as f32, obj.tangent.y as f32));
+            let normal = glm::normalize(&glm::vec2(-obj.tangent.y as f32, obj.tangent.x as f32));
+            let loc = obj.loc;
+
+            let state = instant.infrastructure.object_state.get(pta).cloned().unwrap_or(vec![]);
+
+            use crate::document::objects::*;
+            for f in obj.functions.iter() {
+                match f {
+                    Function::Detector => {
+                        let scale = 0.1;
+                        let a = loc + scale*normal;
+                        let b = loc - scale*normal;
+                        writeln!(output, "\\draw [detector] ({},{}) -- ({},{});",
+                            a.x, a.y, b.x, b.y);
+                    }
+                    Function::MainSignal { .. } => {
+                        let scale = 0.1;
+                        // base
+                        let a = loc - scale*normal;
+                        let b = loc + scale*normal;
+                        writeln!(output,"\\draw [signal] ({},{}) -- ({},{});", a.x, a.y, b.x, b.y);
+                        // stem
+                        let a = loc ;
+                        let b = loc + 1.0*scale*tangent;
+                        writeln!(output,"\\draw [signal] ({},{}) -- ({},{});", a.x, a.y, b.x, b.y);
+                        // light
+                        let green = state.iter().any(|s| matches!(s, ObjectState::SignalProceed));
+                        let a = loc + 2.0*scale*tangent;
+                        let rad = scale;
+                        writeln!(output,"\\draw [signal,{}] ({},{}) circle ({});",
+                                if green { "fill=none" } else { "fill=black" },
+                                a.x, a.y, rad);
+
+                    },
+                    _ => {},
+                }
+            }
+        }
+
+        // trains trains
+        //
+        for t in instant.trains.iter() {
+            use std::collections::HashSet;
+            let mut lines :HashSet<((i32,i32),(i32,i32))> = t.lines.iter().map(|(p1,p2)| 
+                (((p1.x*10000.0) as i32, (p1.y*10000.0) as i32),
+                 ((p2.x*10000.0) as i32, (p2.y*10000.0) as i32))).collect();
+
+            if lines.len() == 0 { continue; }
+
+            let l1 = lines.iter().next().unwrap().clone();
+            lines.remove(&l1);
+            let mut current_pline = vec![l1.0,l1.1];
+
+            while lines.len() > 0 {
+                for (p1,p2) in lines.iter() {
+                    if &current_pline[0] == p2 {
+                        let (p1,p2) = lines.take(&(*p1,*p2)).unwrap();
+                        current_pline.insert(0, p1);
+                        break;
+                    } else if &current_pline[0] == p1 {
+                        let (p1,p2) = lines.take(&(*p1,*p2)).unwrap();
+                        current_pline.insert(0, p2);
+                        break;
+                    } else if &current_pline[current_pline.len()-1] == p1 {
+                        let (p1,p2) = lines.take(&(*p1,*p2)).unwrap();
+                        current_pline.push(p2);
+                        break;
+                    } else if &current_pline[current_pline.len()-1] == p2 {
+                        let (p1,p2) = lines.take(&(*p1,*p2)).unwrap();
+                        current_pline.push(p1);
+                        break;
+                    } 
+                }
+            }
+
+            let coords = current_pline.into_iter().map(|p| format!("({},{})", p.0 as f32 / 10000.0, 
+                                                                   p.1 as f32 / 10000.0))
+                .collect::<Vec<_>>();
+
+            writeln!(output, "\\draw [train] {};", coords.join(" -- "));
+        }
+
+        writeln!(output, "\\end{{tikzpicture}}");
+    }
+
+    std::fs::write("anim.tex", output).unwrap();
+}
+
+
+
+
+
+
+
 
